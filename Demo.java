945e2f6cdbd404f7fb2ce150b4a3244dfbfeac7e
pankaj-anadure <pankaj-anadure@wissen.com>
2022-06-06 17:38:03 +0530

    Final Code

--- Demo.java
+++ Demo.java
@@ -2,7 +2,7 @@ public class Demo
 {
 	public void greet()
 	{
-		System.out.println("Hi ! Good Evening Everyone");
+		System.out.println("Hi ! Good Evening everyone");
 		System.out.println("Have a good day :) ");
 	}
 }
